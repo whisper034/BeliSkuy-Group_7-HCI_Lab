80d308e98744b657fe691bb33d09f99c2c9e4e12
Ariel Sefrian <ariel.sefrian@binus.ac.id>
2022-12-28 19:08:50 +0700

    #PROD initiate
    1. header, navbar, footer done.

---
+++ scripts/header-navbar-footer.js
@@ -0,0 +1,111 @@
+// * buat ngambil header, navBar, dan footer
+
+let headerTake = document.getElementById("header");
+headerTake.innerHTML = 
+`
+<div class="logo">
+<img src="../assets/logos/Logo.png" alt="logo_BeliSkuy_Group_7" width="150rem">
+</div>
+`;
+
+let navTake = document.getElementById("nav");
+navTake.innerHTML = 
+`
+<div class="navbar">
+<a href="../pages/gallery.html">Gallery</a>
+<a href="../pages/register.html">Register</a>
+<a href="../pages/cart.html">Cart</a>
+<a href="../pages/chat.html">Chat</a>
+<a href="../pages/profile.html">Profile</a>
+</div>
+`;
+
+let footerTake = document.getElementById("footer");
+footerTake.innerHTML = 
+`
+<div class="footer">
+      <div class="page">
+        <div class="page-text">
+          <h3>Pages</h3>
+        </div>
+
+        <a href="../pages/gallery.html">
+          <div class="footer-text">
+            Gallery
+          </div>
+        </a>
+
+        <a href="../pages/register.html">
+          <div class="footer-text">
+            Register
+          </div>
+        </a>
+
+        <a href="../pages/cart.html">
+          <div class="footer-text">
+            Cart
+          </div>
+        </a>
+
+        <a href="../pages/chat.html">
+          <div class="footer-text">
+            Chat
+          </div>
+        </a>
+
+        <a href="../pages/profile.html">
+          <div class="footer-text">
+            Profile
+          </div>
+        </a>
+      </div>
+
+      <div class="about-us">
+        <div class="about-us-text">
+          <h3>About Us</h3>
+        </div>
+
+        <a href="">
+          <div class="footer-text">
+            FAQ
+          </div>
+        </a>
+
+        <a href="">
+          <div class="footer-text">
+            Return Policy
+          </div>
+        </a>
+
+        <a href="">
+          <div class="footer-text">
+            Contact Us
+          </div>
+        </a>
+      </div>
+
+      <div class="social-media">
+        <div class="social-media-text">
+          <h3>Social Media</h3>
+        </div>
+
+        <div class="social-media-logo">
+          <a href="https://id-id.facebook.com/" target="_blank">
+            <img src="../assets/logos/facebook.png" alt="facebook_logo" width="30rem">
+          </a>
+
+          <a href="https://instagram.com/patrickjonathan99?igshid=YmMyMTA2M2Y=" target="_blank">
+            <img src="../assets/logos/instagram.png" alt="instagram_logo" width="30rem">
+          </a>
+
+          <a href="https://twitter.com/GenshinImpact?t=UQmk7NOcrwmznGodTsXFYA&s=09" target="_blank">
+            <img src="../assets/logos/twitter.png" alt="twitter_logo" width="30rem">
+          </a>
+
+          <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+            <img src="../assets/logos/youtube.png" alt="youtube_logo" width="30rem">
+          </a>
+        </div>
+      </div>
+    </div>
+`;
