cfde63662392ed46d0cb0812317663490f2d25b9
Ariel Sefrian <ariel.sefrian@binus.ac.id>
2023-01-07 22:14:00 +0700

    #2 register javascript validation
    1. added thank you page along with its css and script
    2. added the images for gallery and category for gallery page later
    3. added jquery v3.6.3 for the gallery page later
    4. change input type text to date for date of birth

---
+++ scripts/register.js
@@ -0,0 +1,77 @@
+const registerHandler = () => {
+  // langsung didefine aja semua value yang ada
+  const getSelectedGenderValue = document.querySelector("input[name='gender']:checked") == null ? null : document.querySelector("input[name='gender']:checked").value;
+  const getFirstNameValue = document.getElementById("first_name").value;
+  
+  const getDateOfBirthValue = document.getElementById("dob").value;
+  var today = new Date();
+  var birthDate = new Date(getDateOfBirthValue);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  const getUsernameValue = document.getElementById("username").value;
+  const getEmailValue = document.getElementById("email").value;
+  const getConfirmEmailValue = document.getElementById("confirm_email").value;
+  const getPasswordValue = document.getElementById("password").value;
+  const getConfirmPasswordValue = document.getElementById("confirm_password").value;
+  const getCheckedTermsAndConditionsValue = document.querySelector("#terms_and_conditions");
+  
+
+  let canBeSubmitted = true;
+  // pake if dan else if secara berurut biar alertnya gk banyak
+  if (!getSelectedGenderValue) {
+    canBeSubmitted = false;
+    alert("Please select your gender.");
+  }
+  else if (!getFirstNameValue) { // last name tidak wajib diisi
+    canBeSubmitted = false;
+    alert("Please input your first name.");
+  }
+  else if (!getDateOfBirthValue) {
+    canBeSubmitted = false;
+    alert("Please select your birth date.");
+  }
+  else if (getDateOfBirthValue && age < 0) {
+    canBeSubmitted = false;
+    alert("That's not even possible, right? Your age is " + age + " you know?");
+  }
+  else if (!getUsernameValue) {
+    canBeSubmitted = false;
+    alert("Please input your username.");
+  }
+  else if (!getEmailValue) {
+    canBeSubmitted = false;
+    alert("Please input your email.");
+  }
+  else if (!getConfirmEmailValue) {
+    canBeSubmitted = false;
+    alert("Please confirm your email.");
+  }
+  else if (getEmailValue !== getConfirmEmailValue) {
+    canBeSubmitted = false;
+    alert("Your email doesn't match. Please try again.");
+  }
+  else if (!getPasswordValue) {
+    canBeSubmitted = false;
+    alert("Please make a password.");
+  }
+  else if (!getConfirmPasswordValue) {
+    canBeSubmitted = false;
+    alert("Please confirm your password.");
+  }
+  else if (getPasswordValue !== getConfirmPasswordValue) {
+    canBeSubmitted = false;
+    alert("Your password doesn't match. Please try again.");
+  }
+  else if (!getCheckedTermsAndConditionsValue.checked) {
+    canBeSubmitted = false;
+    alert("You have to agree with all of our terms and conditions first.");
+  }
+
+  if (canBeSubmitted) {
+    location.replace("../pages/thank-you.html");
+  }
+}
